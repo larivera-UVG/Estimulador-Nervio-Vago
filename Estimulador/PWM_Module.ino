c4e7ce627552c466d35504b35fb683ba75b2873f
Gustavo Ordoñez <63011484+Gustavo08123@users.noreply.github.com>
2020-07-20 19:11:02 -0600

    Funciones

--- Estimulador/PWM_Module.ino
+++ Estimulador/PWM_Module.ino
@@ -1,9 +1,5 @@
 
-//............................................................................................
-
-bool state = 0;
- 
-// ............................. PWM and Timer Formulas ...........................................
+// ............................................. PWM and Timer Formulas ................................................
 
 // PWM frequency:
 // freq = GCLKx_freq/(TCCx_Prescaler*(1+TOP_Value))
@@ -15,16 +11,16 @@ bool state = 0;
 
 // Note: dfactor is a scaling factor selected in clock divider to GCLKx configuration
 
-// ................................................................................................
+
+// ....................................... PWM and Timer Values Data Base...............................................
 
 
-// Array for frequency configuration or top value for a 30, 25, 20, 15, 10, 5, 2 and 1Hz 
-// repectively, of the Stimulation Signal. 
+// Frequency configuration of the Stimulation Signal (PWM) for 30, 25, 20, 15, 10, 5, 2 and 1Hz  
 const unsigned int PWM_freq[3][8] = {{1561, 1874, 2343, 3124, 4687, 9374, 23437, 46874},    // 48MHz
                                      {2082, 2499, 3124, 4166, 6249, 12499, 31249, 62499},   // 8MHz
                                      {1066, 1279, 1599, 2132, 3199, 6399, 15999, 31999}};   // 32KHz
 
-// Array for CCx values for 1ms, 750us, 500us, 250us and 130us respectively, for pulse 
+// CCx values for 1ms, 750us, 500us, 250us and 130us respectively, for pulse 
 // width or hight state in the square signal (PWM) during the Stimulation Time
 const unsigned char PWM_pw[3][5] = {{47, 35, 23, 12, 6},   // 48MHz
                                     {62, 47, 31, 16, 8},   // 8MHz
@@ -35,10 +31,19 @@ const unsigned char PWM_pw[3][5] = {{47, 35, 23, 12, 6},   // 48MHz
 const unsigned int ON_OFF_TIME[14] = {18749, 9374, 5624, 3374, 2062, 1874, 1499, 937, 655, 562, 437, 218, 62, 30};
 
 
-volatile unsigned int period = PWM_freq[0][5];
-volatile float pulse_width = period*0.5;
+// ........................................ PWM and Timer Values Selection .............................................
+
+
+volatile unsigned int period = PWM_freq[0][0];
+volatile float pulse_width = period*0.5;  //PWM_pw[0][4];
 volatile int ON_Time = ON_OFF_TIME[13];
-volatile int OFF_Time = ON_OFF_TIME[13];
+volatile int OFF_Time = ON_OFF_TIME[12];
+
+
+// ............................... Variables for PWM, Timer and Digital Potentiometer ..................................
+
+
+bool state = 0;
 
 // Select Gen Clock to setting the waveform generator clock or sample rate
 const unsigned char gClock = 4;
@@ -46,92 +51,58 @@ const unsigned char gClock = 4;
 // Set the divide factor for the Gen Clock, 48MHz/1 = 48MHz
 const unsigned char dFactor = 1; 
 
+const int UD = 1;
+const int INC = 2;
+const int CS = 3;
+
+int val, val1;
+
+// .....................................................................................................................
+
+
 void setup()
 {
 
-  pinMode(0, OUTPUT);
-
-
-//.............................. Configurare PWM (Stimulation signal) ..........................
-
+//...................................... Set Input/Output Pins Mode Configuration ......................................
   
-  // Enable and configure the Generic CLK Generator (GCLK)
-  REG_GCLK_GENCTRL = GCLK_GENCTRL_IDC |               // Improve duty cycle 
-                     GCLK_GENCTRL_GENEN |             // Enable GCLK
-                     GCLK_GENCTRL_SRC_DFLL48M |       // Set the 48MHz as Clock Source
-                     GCLK_GENCTRL_ID(gClock);         // Select GCLK4 as ID 
-  while (GCLK->STATUS.bit.SYNCBUSY);                  // Wait for synchronization
+  pinMode(0, OUTPUT);               // VNS Stimulation Signal Output
+  pinMode(UD, OUTPUT);              // U/'D
+  pinMode(INC, OUTPUT);             // 'INC
+  pinMode(CS, OUTPUT);              // 'CS
+  //analogWriteResolution(10);
+  digitalWrite(CS, HIGH);  
+  digitalWrite(INC, HIGH); 
+  digitalWrite(UD, HIGH);
 
+  setResistance(61);
 
-  // Select clock divider to GCLK4
-  REG_GCLK_GENDIV = GCLK_GENDIV_DIV(dFactor) |        // Divide 48MHz by 1
-                    GCLK_GENDIV_ID(gClock);           // Apply it to GCLK4 
-  while (GCLK->STATUS.bit.SYNCBUSY);                  // Wait for synchronization
+//...................................... Configurare PWM (Stimulation signal) ..........................................
 
+  PWM_Config();
+  //PWM_Start();
 
-  // Enable GCLK4 and connect it to TCC0 and TCC1
-  REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |             // Enable Generic Clock 
-                     GCLK_CLKCTRL_GEN_GCLK4 |         // Select GCLK4
-                     GCLK_CLKCTRL_ID_TCC0_TCC1;       // Feed CLK4 to TCC0 and TCC1
-  while (GCLK->STATUS.bit.SYNCBUSY);                  // Wait for synchronization
+//...................................... Configurar Timer (ON/OFF Time) ................................................
 
+  Timer_Config();           
+  Timer_Start();    
 
-  // Divide counter by 1 (This is N)
-  TCC0->CTRLA.reg |= TCC_CTRLA_PRESCALER(TCC_CTRLA_PRESCALER_DIV1024_Val);
-  //TCC0->CTRLA.reg |= TCC_CTRLA_PRESCALER_DIV1;
+// .....................................................................................................................
 
-  // Set Timer counter Mode to 16 bits
-  TCC0->CTRLA.reg |= TC_CTRLA_MODE_COUNT16;
-
-  // Use Normal PWM (Single-slope): count up to PER, match on CC[n]
-  TCC0->WAVE.reg = TCC_WAVE_WAVEGEN_NPWM;             // Select NPWM as waveform
-  while (TCC0->SYNCBUSY.bit.WAVE);                    // Wait for synchronization
-
-  // Set the period (TOP) before resetting timer
-  TCC0->PER.reg = period;                              // Set the rate or frequency of PWM signal
-  while (TCC0->SYNCBUSY.bit.PER);                      // Wait for synchronization
-
-  // Set duty cycle where n for CC[n] is n = x % 4 and x is form WO[x]
-  TCC0->CC[0].reg = pulse_width;                      // Set PWM signal to 50% of duty cicle 
-  while (TCC0->SYNCBUSY.bit.CC0);                     // Wait for synchronization
-
-  // Configure PA08 (D0 in Trinket) to be output
-  PORT->Group[PORTA].DIRSET.reg = PORT_PA08;          // Set pin as output
-  PORT->Group[PORTA].OUTCLR.reg = PORT_PA08;          // Set pin to low
-
-  // Enable the port multiplexer for PA08
-  PORT->Group[PORTA].PINCFG[8].reg |= PORT_PINCFG_PMUXEN;
-
-  // Connet TCC1 timer to PA08. Function F is TCC1/WO[2] for PA08
-  // Odd pin num (2*n+1): use PMUXO
-  // Even pin num (2*n): use PMUXE
-  PORT->Group[PORTA].PMUX[4].reg = PORT_PMUX_PMUXE_E;
-
-//  REG_TCC1_INTENSET = TCC_INTENSET_OVF;
-//  enable_interrupts();
-
-
-//  // Enable TCC1 to start (Start PWM)
-////  REG_TCC1_CTRLA |= TCC_CTRLA_PRESCALER_DIV1 |        // Divide GCLK4 by 1
-////                    TCC_CTRLA_ENABLE;                 // Enable the TCC0 output
-//  TCC0->CTRLA.reg |= (TCC_CTRLA_ENABLE);              // Enable TCC1 output
-//  while (TCC0->SYNCBUSY.bit.ENABLE);                  // Wait for synchronization
-
-
-//.............................. Configurar Timer (ON/OFF Time) ................................
-
-  tcConfigure();          //configure the timer to run at <sampleRate>Hertz
-  tcStartCounter();    
-
-//..............................................................................................
+  //DAC_Init();
 
 }
 
 
 void loop() 
 {
-
+//  val = digitalRead(0);
+//  val1 = val*530;
+//  DAC->DATA.reg = (val1);                         // Write DAC value
+//  while(DAC->STATUS.reg & DAC_STATUS_SYNCBUSY);   // Make sure clock synchronization 
 }
+
+
+// .....................................................................................................................
 
 
 //void enable_interrupts() 
@@ -152,46 +123,148 @@ void loop()
 //
 //}
 
-//............................................................................................
+
+// .....................................................................................................................
+
+
+void setResistance(int percent){ 
+  digitalWrite(UD, LOW);                // Select Decrement Counter
+  digitalWrite(CS, LOW);                // Select Digital Potenciometer
+    
+  for (int i=0; i<100; i++){            // Increment/Decrement Counter
+    digitalWrite(INC, LOW);             // Low State (this gives a negative edge)
+    delayMicroseconds(1);               
+    digitalWrite(INC, HIGH);            // High State 
+    delayMicroseconds(1);
+  }
+
+  digitalWrite(UD, HIGH);               // Select Increment Counter
+  for (int i=0; i<percent; i++){        // Increment/Decrement Counter
+    digitalWrite(INC, LOW);             // Low State (this gives a negative edge)
+    delayMicroseconds(1);
+    digitalWrite(INC, HIGH);            // High State 
+    delayMicroseconds(1);
+  }
+
+  digitalWrite(CS, HIGH);               // Save counter in non-volatile memory and enter in Stand-By Mode
+}
+
+
+// .....................................................................................................................
 
 
 void TC5_Handler (void) {
   
   if(state == true) 
   {
-    // Enable TCC1 to start (Start PWM)
-    TC5->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE;         // Stop Timer to change to ON time value
-    while (tcIsSyncing());    
-    
-    TCC0->CTRLA.reg |= (TCC_CTRLA_ENABLE);              // Enable TCC1 (Start PWM)
-    while (TCC0->SYNCBUSY.bit.ENABLE);                  // Wait for synchronization
-  
+    Timer_Disable();                                    // Stop Timer to change to ON time value       
     TC5->COUNT16.CC[0].reg = (uint16_t) ON_Time;        // Set TC5 value with ON Time (Stimulation)
-    TC5->COUNT16.CTRLA.reg |= TC_CTRLA_ENABLE;          // Enable Timer again 
-    while (tcIsSyncing());
+    Timer_Start();                                      // Enable Timer again 
+    PWM_Start();                                        // Enable PWM 
   } 
     else 
   {
-    TC5->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE;         // Stop Timer to change to OFF time value
-    while (tcIsSyncing());
-    
-    TCC0->CTRLA.reg &= (~TCC_CTRLA_ENABLE);             // Disable TCC1 (Stop PWM)
-    while (TCC0->SYNCBUSY.bit.ENABLE);                  // Wait for synchronization
-    digitalWrite(0, LOW);
-  
-    TC5->COUNT16.CC[0].reg = (uint16_t) OFF_Time;       // Set TC5 value with OFF Time (Repose)
-    TC5->COUNT16.CTRLA.reg |= TC_CTRLA_ENABLE;          // Enable Timer again 
-    while (tcIsSyncing()); 
-  }
-  
-  state = !state;
+    Timer_Disable();                                    // Stop Timer to change to OFF time value
+    TC5->COUNT16.CC[0].reg = (uint16_t) OFF_Time;       // Set TC5 value with OFF Time (Repose)    
+    Timer_Start();                                      // Enable Timer again  
+    PWM_Stop();                                         // Disable PWM
+    digitalWrite(0, LOW);                   
+  }  
+  state = !state;                                       // ON Time <-> OFF Time
   TC5->COUNT16.INTFLAG.bit.MC0 = 1;                     // Clear interrupt flag
+  
 }
 
 
+// .....................................................................................................................
 
 
-void tcConfigure()
+void PWM_Config()
+{
+
+  // Enable and configure the Generic CLK Generator (GCLK)
+  REG_GCLK_GENCTRL = GCLK_GENCTRL_IDC |               // Improve duty cycle 
+                     GCLK_GENCTRL_GENEN |             // Enable GCLK
+                     GCLK_GENCTRL_SRC_DFLL48M |       // Set the 48MHz as Clock Source
+                     GCLK_GENCTRL_ID(gClock);         // Select GCLK4 as ID 
+  while (GCLK->STATUS.bit.SYNCBUSY);                  // Wait for synchronization
+
+
+  // Select clock divider to GCLK4
+  REG_GCLK_GENDIV = GCLK_GENDIV_DIV(dFactor) |        // Divide 48MHz by 1
+                    GCLK_GENDIV_ID(gClock);           // Apply it to GCLK4 
+  while (GCLK->STATUS.bit.SYNCBUSY);                  // Wait for synchronization
+
+
+  // Enable GCLK4 and connect it to TCC0 and TCC1
+  REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |             // Enable Generic Clock 
+                     GCLK_CLKCTRL_GEN_GCLK4 |         // Select GCLK4
+                     GCLK_CLKCTRL_ID_TCC0_TCC1;       // Feed CLK4 to TCC0 and TCC1
+  while (GCLK->STATUS.bit.SYNCBUSY);                  // Wait for synchronization
+
+  TCC0->CTRLA.reg |= TCC_CTRLA_PRESCALER(TCC_CTRLA_PRESCALER_DIV1024_Val);    // Divide counter by 1 (This is N)
+  //TCC0->CTRLA.reg |= TCC_CTRLA_PRESCALER_DIV1;
+
+  TCC0->CTRLA.reg |= TC_CTRLA_MODE_COUNT16;           // Set Timer counter Mode to 16 bits
+
+  TCC0->WAVE.reg = TCC_WAVE_WAVEGEN_NPWM;             // Select NPWM (Single-slope): count up to PER, match on CC[n]
+  while (TCC0->SYNCBUSY.bit.WAVE);                    // Wait for synchronization
+
+  TCC0->PER.reg = period;                              // Set the period (TOP) for rate or frequency of PWM signal
+  while (TCC0->SYNCBUSY.bit.PER);                      // Wait for synchronization
+
+  // Set duty cycle where n for CC[n] is n = x % 4 and x is form WO[x]
+  TCC0->CC[0].reg = pulse_width;                      // Set PWM signal to 50% of duty cicle 
+  while (TCC0->SYNCBUSY.bit.CC0);                     // Wait for synchronization
+
+  // Configure PA08 (D0 in Trinket) to be output
+  PORT->Group[PORTA].DIRSET.reg = PORT_PA08;          // Set pin as output
+  PORT->Group[PORTA].OUTCLR.reg = PORT_PA08;          // Set pin to low
+
+  // Enable the port multiplexer for PA08
+  PORT->Group[PORTA].PINCFG[8].reg |= PORT_PINCFG_PMUXEN;
+
+  // Connet TCC1 timer to PA08. Function F is TCC1/WO[2] for PA08. Function E is TCC0/WO[0]
+  // Odd pin num (2*n+1): use PMUXO
+  // Even pin num (2*n): use PMUXE
+  PORT->Group[PORTA].PMUX[4].reg = PORT_PMUX_PMUXE_E;
+
+//  REG_TCC1_INTENSET = TCC_INTENSET_OVF;
+//  enable_interrupts();
+
+//// Enable TCC1 to start (Start PWM)
+//// REG_TCC1_CTRLA |= TCC_CTRLA_PRESCALER_DIV1 |        // Divide GCLK4 by 1
+////                    TCC_CTRLA_ENABLE;                 // Enable the TCC0 output
+//  TCC0->CTRLA.reg |= (TCC_CTRLA_ENABLE);              // Enable TCC1 output
+//  while (TCC0->SYNCBUSY.bit.ENABLE);                  // Wait for synchronization
+  
+}
+
+
+// .....................................................................................................................
+
+
+void PWM_Start()
+{ 
+  TCC0->CTRLA.reg |= (TCC_CTRLA_ENABLE);              // Enable TCC1 (Start PWM)
+  while (TCC0->SYNCBUSY.bit.ENABLE);                  // Wait for synchronization     
+}
+
+
+// .....................................................................................................................
+
+
+void PWM_Stop()
+{
+  TCC0->CTRLA.reg &= (~TCC_CTRLA_ENABLE);             // Disable TCC1 (Stop PWM)
+  while (TCC0->SYNCBUSY.bit.ENABLE);                  // Wait for synchronization
+}
+
+
+// .....................................................................................................................
+
+
+void Timer_Config()
 {
 
   // Enable and configure the Generic CLK Generator (GCLK)
@@ -212,7 +285,7 @@ void tcConfigure()
                      GCLK_CLKCTRL_ID_TC4_TC5;           // Feed CLK5 to TC4 and TC5
   while (GCLK->STATUS.bit.SYNCBUSY);                    // Wait for synchronization
  
-  tcReset(); //reset TC5
+  Timer_Reset(); //reset TC5
 
   // Set TC5 Mode, WaveForm, Prescaler and Enable it 
   TC5->COUNT16.CTRLA.reg |= TC_CTRLA_MODE_COUNT16;      // Set Timer counter mode to 16 bits
@@ -233,6 +306,9 @@ void tcConfigure()
 } 
 
 
+// .....................................................................................................................
+
+
 // Check if TC5 syncing is done (true)
 bool tcIsSyncing()
 {
@@ -240,15 +316,21 @@ bool tcIsSyncing()
 }
 
 
+// .....................................................................................................................
+
+
 // Enables TC5 and waits for it to be ready
-void tcStartCounter()
+void Timer_Start()
 {
   TC5->COUNT16.CTRLA.reg |= TC_CTRLA_ENABLE; //set the CTRLA register
   while (tcIsSyncing()); //wait until snyc'd
 }
 
 
-void tcReset()
+// .....................................................................................................................
+
+
+void Timer_Reset()
 {
   TC5->COUNT16.CTRLA.reg = TC_CTRLA_SWRST;
   while (tcIsSyncing());
@@ -256,8 +338,42 @@ void tcReset()
 }
 
 
-void tcDisable()
+// .....................................................................................................................
+
+
+void Timer_Disable()
 {
   TC5->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE;
   while (tcIsSyncing());
 }
+
+
+// .....................................................................................................................
+
+
+void DAC_Init(void)
+{  
+  PORT->Group[PORTA].DIRSET.reg = PORT_PA02;                  // Configure PA02 (D1 in Trinket M0) to be output
+  PORT->Group[PORTA].PINCFG[2].reg |= PORT_PINCFG_PMUXEN;     // Enable the port multiplexer for PA02
+
+  // Odd pin num (2*n+1): use PMUXO
+  // Even pin num (2*n): use PMUXE
+  PORT->Group[PORTA].PMUX[1].reg = PORT_PMUX_PMUXE_B;         // Connet DAC to PA02. Function B is DAC OUTPUT for PA02      
+  
+  PM->APBCMASK.reg |= PM_APBCMASK_DAC;                        // Enable Peripheral Clock for DAC
+  
+  GCLK->CLKCTRL.reg = GCLK_CLKCTRL_ID (DAC_GCLK_ID) |         // DAC share same GCLK
+                      GCLK_CLKCTRL_CLKEN |                    // Enable Generic Clock
+                      GCLK_CLKCTRL_GEN(0);                    // Select Generic Clock #0
+  
+  DAC->CTRLA.reg = DAC_CTRLA_SWRST;                           // Apply Software Reset
+  while (DAC->CTRLA.reg & DAC_CTRLA_SWRST);                   // Wait Until Reset is Complete
+ 
+  DAC->CTRLA.reg &= ~(DAC_CTRLA_ENABLE);                      // Disable DAC to permit change CTRLA B register.
+  DAC->CTRLB.reg |= DAC_CTRLB_EOEN | DAC_CTRLB_REFSEL_AVCC;   // Enable the DAC output on Vout pin. Enable VDDANAL REF. 
+  DAC->CTRLA.reg |= DAC_CTRLA_ENABLE;                         // Enable DAC
+  while (DAC->STATUS.reg & DAC_STATUS_SYNCBUSY);              // Wait Until Clock is Ok
+}
+
+
+// .....................................................................................................................
