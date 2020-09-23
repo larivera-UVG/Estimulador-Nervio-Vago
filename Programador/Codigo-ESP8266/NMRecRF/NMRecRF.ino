7f1b08889f2cae04f376cb8a24b084d66596f750
MikeAlvarezs <40964939+MikeAlvarezs@users.noreply.github.com>
2020-09-22 22:00:38 -0600

    Cambios códigos transmisor y receptor
    Receptor: se añadió la capacidad de eliminar (no tomar en cuenta) los envíos múltiples.
    
    Transmisor:  se añadió el soporte para comunicarse con la interfaz gráfica.

--- Programador/Codigo-ESP8266/NMRecRF/NMRecRF.ino
+++ Programador/Codigo-ESP8266/NMRecRF/NMRecRF.ino
@@ -8,22 +8,93 @@
 
 RCSwitch mySwitch = RCSwitch();
 
+int i = 0; 
+int k = 0;
+int value; 
+int slcond = 0; 
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   mySwitch.enableReceive(0);  // Receiver on interrupt 0 => that is pin #2
 }
 
 void loop() {
   if (mySwitch.available()) {
-    
-    Serial.print("Received ");
-    Serial.print( mySwitch.getReceivedValue() );
-    Serial.print(" / ");
-    Serial.print( mySwitch.getReceivedBitlength() );
-    Serial.print("bit ");
-    Serial.print("Protocol: ");
-    Serial.println( mySwitch.getReceivedProtocol() );
+
+    if(i == 0 & k == 0){
+      value = mySwitch.getReceivedValue();
+      Serial.print("Modo: ");
+      if(value == 1){
+        Serial.println("Estimulación");
+      }
+      else if(value == 2){
+        Serial.println("Iman");
+      }
+      else if(value == 3){
+        Serial.println("Reposo");
+      }
+      //Serial.print("Modo:  ");
+      //Serial.println( value );
+      //Serial.println("");
+    }
+    else if(i == 0 & k == 1){
+      value = mySwitch.getReceivedValue();
+      Serial.print("Ancho de pulso:  ");
+      Serial.println( value );
+      //cond2
+      //Serial.println("");
+    }
+    else if(i == 0 & k == 2){
+      value = mySwitch.getReceivedValue();
+      Serial.print("Tiempo de estimulación:  ");
+      Serial.println( value );
+      //Serial.println("");
+    }
+    else if(i == 0 & k == 3){
+      value = mySwitch.getReceivedValue();
+      Serial.print("Frecuencia:  ");
+      Serial.println( value );
+     // Serial.println("");
+    }
+    else if(i == 0 & k == 4){
+      value = mySwitch.getReceivedValue();
+      Serial.print("Tiempo de sleep:  ");
+      Serial.println( value );
+      //Serial.println("");
+    }
+    else if(i == 5 & k == 4){
+      slcond = 1; 
+    }
 
     mySwitch.resetAvailable();
+    i++;
+    
+  }
+
+  
+  if(i == 4 & k == 0){
+    i = 0;
+    k = 1;
+  }
+  else if(i == 5 & k == 1){
+    i = 0;
+    k = 2;
+  }
+  else if(i == 5 & k == 2){
+    i = 0;
+    k = 3;
+  }
+  else if(i == 5 & k == 3){
+    i = 0;
+    k = 4;
+  }
+  else if(i == 5 & k == 4){
+    i = 0;
+    k = 5;
+    slcond = 0;
+  }
+
+  if(k == 5){
+    k = 0;
   }
 }
