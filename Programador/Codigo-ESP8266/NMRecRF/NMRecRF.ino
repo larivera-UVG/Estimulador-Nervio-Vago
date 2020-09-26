2def19db8b70ed4e2f19a47b0cda83a6c2664889
MikeAlvarezs <40964939+MikeAlvarezs@users.noreply.github.com>
2020-09-26 17:32:06 -0600

    Update NMRecRF.ino

--- Programador/Codigo-ESP8266/NMRecRF/NMRecRF.ino
+++ Programador/Codigo-ESP8266/NMRecRF/NMRecRF.ino
@@ -33,39 +33,86 @@ void loop() {
       else if(value == 3){
         Serial.println("Reposo");
       }
-      //Serial.print("Modo:  ");
-      //Serial.println( value );
-      //Serial.println("");
     }
     else if(i == 0 & k == 1){
       value = mySwitch.getReceivedValue();
-      Serial.print("Ancho de pulso:  ");
-      Serial.println( value );
-      //cond2
-      //Serial.println("");
+      Serial.print("Ancho de pulso: ");
+      if(value == 1){
+        Serial.println("25%");
+      }
+      else if(value == 2){
+        Serial.println("30%");
+      }
+      else if(value == 3){
+        Serial.println("50%");
+      }
+      else if(value == 4){
+        Serial.println("75%");
+      }
+      else if(value == 5){
+        Serial.println("100%");
+      }
     }
     else if(i == 0 & k == 2){
       value = mySwitch.getReceivedValue();
       Serial.print("Tiempo de estimulación:  ");
-      Serial.println( value );
-      //Serial.println("");
+      if(value == 1){
+        Serial.println("Tiempo 1");
+      }
+      else if(value == 2){
+        Serial.println("Tiempo 2");
+      }
+      else if(value == 3){
+        Serial.println("Tiempo 3");
+      }
+      else if(value == 4){
+        Serial.println("Tiempo 4");
+      }
+      else if(value == 5){
+        Serial.println("Tiempo 5");
+      }
     }
     else if(i == 0 & k == 3){
       value = mySwitch.getReceivedValue();
       Serial.print("Frecuencia:  ");
-      Serial.println( value );
-     // Serial.println("");
+      if(value == 1){
+        Serial.println("490.20 Hz (Por defecto)");
+      }
+      else if(value == 2){
+        Serial.println("30.64 Hz");
+      }
+      else if(value == 3){
+        Serial.println("122.50 Hz");
+      }
+      else if(value == 4){
+        Serial.println("245.10 Hz");
+      }
+      else if(value == 5){
+        Serial.println("980.39 Hz");
+      }
+      else if(value == 6){
+        Serial.println("3921.16 Hz");
+      }
+      else if(value == 7){
+        Serial.println("31372.55 Hz");
+      }
+      else if(value == 8){
+        Serial.println("Frecuencia 8");
+      }
     }
     else if(i == 0 & k == 4){
       value = mySwitch.getReceivedValue();
       Serial.print("Tiempo de sleep:  ");
-      Serial.println( value );
-      //Serial.println("");
+      if(value == 1){
+        Serial.println("Sleep 1");
+      }
+      else if(value == 2){
+        Serial.println("Sleep 2");
+      }
     }
     else if(i == 5 & k == 4){
       slcond = 1; 
     }
-
     mySwitch.resetAvailable();
     i++;
     
@@ -96,5 +143,6 @@ void loop() {
 
   if(k == 5){
     k = 0;
+    Serial.println("");
   }
 }
